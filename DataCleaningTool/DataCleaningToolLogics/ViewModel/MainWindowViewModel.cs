44ec318c3fcde226fe51c7bbb327559bd8a2e6af
Jacwing <Adam.Jacwing@op.pl>
2016-09-06 23:59:52 +0200

    DataCleaningTool - Testing DataContext.

---
+++ DataCleaningTool/DataCleaningToolLogics/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using SimpleMVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DataCleaningToolLogic.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        #region fields
+
+        private string buttonCaption;
+        private ICommand buttonClickCommand;
+
+        #endregion
+
+        #region properties
+
+        public string ButtonCaption
+        {
+            get { return buttonCaption; }
+            set
+            {
+                SetProperty(ref buttonCaption, value);
+            }
+        }
+
+        #endregion
+
+        #region commands
+
+        public ICommand ButtonClickCommand
+        {
+            get { return buttonClickCommand; }
+        }
+
+        #endregion
+
+        #region methods
+
+        public MainWindowViewModel()
+        {
+            ButtonCaption = "OK";
+            this.buttonClickCommand = new RelayCommand(ButtonClickAction);
+        }
+
+        private void ButtonClickAction(object obj)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                    MessageBox.Show(filename);
+            }
+        }
+
+
+        #endregion
+    }
+}
